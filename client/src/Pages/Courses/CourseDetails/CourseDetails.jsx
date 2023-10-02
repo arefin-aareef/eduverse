2894815e1d747d5f12984880ea7126247cbc5ab2
Arefin Aareef <arefin.aareef@gmail.com>
2023-10-02 23:11:14 +0600

    course details page

--- client/src/Pages/Courses/CourseDetails/CourseDetails.jsx
+++ client/src/Pages/Courses/CourseDetails/CourseDetails.jsx
@@ -1,17 +1,45 @@
-
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData } from "react-router-dom";
 
 const CourseDetails = () => {
+  const course = useLoaderData();
+  const {
+    course_name,
+    course_description,
+    course_duration,
+    course_content,
+    category,
+    instructor_name,
+    instructor_email,
+    rating,
+    price,
+    review,
+    image,
+    video,
+  } = course;
 
-    const course = useLoaderData();
-    const {course_name} = course
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <div className="hero-overlay bg-opacity-80"></div>
 
-    return (
-        <div className='pt-20'>
-            <p>{course_name}</p>
-            hi
+      <div className="hero-content text-white  pt-20">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            <div>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/gvkqT_Uoahw?si=8wdP6YqacaY6BboK" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
+
+    <div>
+    <h1 className="mb-5 text-3xl font-bold">{course_name}</h1>
+          <p className="mb-5 text-xl">{course_description}</p>
+          <p className="mb-5 text-lg">Content: {course_content}</p>
+          <button className="btn btn-outline text-white">Enroll now</button>
+    </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CourseDetails;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import AuthProvider from "./Providers/AuthProvider";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <div className="max-w-screen-2xl min-h-screen mx-auto">
+      <div className="max-w-screen-2xl min-h-screen mx-auto bg-slate-500">
         <RouterProvider router={router} />
       </div>
     </AuthProvider>
