b689bcefe9e51c184b6da91fcc788b4500c3a4c7
Arefin Aareef <arefin.aareef@gmail.com>
2023-10-03 01:25:54 +0600

    small fixes

--- client/src/Pages/Signup/Signup.jsx
+++ client/src/Pages/Signup/Signup.jsx
@@ -1,63 +1,57 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider";
-import Swal from 'sweetalert2'
-
-
+import Swal from "sweetalert2";
 
 const Signup = () => {
-
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm()
+  } = useForm();
 
-  const {createUser, updateUserProfile} = useContext(AuthContext)
-  const navigate = useNavigate()
-
+  const { createUser, updateUserProfile } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   const onSubmit = (data) => {
-    console.log(data)
-    createUser(data.email, data.password)
-    .then(result => {
-      const loggedUser = result.user
+    console.log(data);
+    createUser(data.email, data.password).then((result) => {
+      const loggedUser = result.user;
       console.log(loggedUser);
-      updateUserProfile(data.name, data.photoURL)
-      .then(() => {
-        const saveUser = {name: data.name, photo: data.photoURL}
+      updateUserProfile(data.name, data.photoURL).then(() => {
+        const saveUser = { name: data.name, photo: data.photoURL };
         console.log(saveUser);
-      })
+      });
       Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: 'User Created Successfully',
+        position: "top-end",
+        icon: "success",
+        title: "User Created Successfully",
         showConfirmButton: false,
-        timer: 1500
-      })
-      navigate('/')
-    })
-  }
+        timer: 1500,
+      });
+      navigate("/");
+    });
+  };
 
-
-    return (
-        <div className="pt-12 lg:pt-16 bg-slate-600 min-h-screen">
+  return (
+    <div className="pt-12 bg-slate-600 min-h-screen">
       <div className="hero pt-12 ">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-
-            
           <div className="text-center lg:text-right  text-white w-full flex flex-col justify-center">
             <h1 className="text-5xl font-bold">Sign up now!</h1>
             <p className="py-6 hidden lg:block">
-              Already have an account? <span className="underline"><Link to='/login'>Login</Link></span> here.
+              Already have an account?{" "}
+              <span className="underline">
+                <Link to="/login">Login</Link>
+              </span>{" "}
+              here.
             </p>
           </div>
-          
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
 
-          <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-          <div className="form-control">
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
                 <label className="label">
                   <span className="label-text">Name</span>
                 </label>
@@ -123,8 +117,8 @@ const Signup = () => {
                 )}
                 {errors.password?.type === "minLength" && (
                   <span className="text-red-600">
-                    Password must be at least 6 characters with one uppercase, one lowercase and one
-                    number
+                    Password must be at least 6 characters with one uppercase,
+                    one lowercase and one number
                   </span>
                 )}
                 {errors.password?.type === "maxLength" && (
@@ -147,18 +141,19 @@ const Signup = () => {
                 />
               </div>
             </form>
-
-
           </div>
 
           <p className=" lg:hidden">
-          Already have an account? <span className="underline"><Link to='/login'>Login</Link></span> here.
-            </p>
-
+            Already have an account?{" "}
+            <span className="underline">
+              <Link to="/login">Login</Link>
+            </span>{" "}
+            here.
+          </p>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Signup;
