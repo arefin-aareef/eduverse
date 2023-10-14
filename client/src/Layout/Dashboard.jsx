e05782a5c4bc781dd3e60083dca3e4ead3bd69aa
Arefin Aareef <arefin.aareef@gmail.com>
2023-10-14 23:07:42 +0600

    small fixes

--- client/src/Layout/Dashboard.jsx
+++ client/src/Layout/Dashboard.jsx
@@ -1,6 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { BiHomeAlt2, BiSolidHome } from 'react-icons/bi';
-import { AiOutlineHeart } from 'react-icons/ai';
+import { AiOutlineHeart, AiOutlineBars } from 'react-icons/ai';
 import { SiGotomeeting } from 'react-icons/si';
 
 const Dashboard = () => {
@@ -8,13 +8,13 @@ const Dashboard = () => {
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col items-center  text-white">
-          <Outlet></Outlet>
-        <label
-          htmlFor="my-drawer-2"
-          className="btn btn-primary drawer-button lg:hidden"
-        >
-          Open drawer
-        </label>
+      <label
+            htmlFor="my-drawer-2"
+            className="btn hover:btn-neutral ms-auto my-2 me-2 drawer-button mb-4 lg:hidden"
+          >
+            <AiOutlineBars />
+          </label>
+            <Outlet></Outlet>
       </div>
       <div className="drawer-side">
         <label
@@ -23,11 +23,11 @@ const Dashboard = () => {
           className="drawer-overlay"
         ></label>
         <ul className="menu p-4 w-80 min-h-full bg-slate-300 text-base-content">
-          <li><Link to="/dashboard/userhome"><BiHomeAlt2 /> User Home</Link></li>
-          <li><Link to="/dashboard/myfavorite"><AiOutlineHeart />My Favorites</Link></li>
+          <li><NavLink to="/dashboard/userhome"><BiHomeAlt2 /> User Home</NavLink></li>
+          <li><NavLink to="/dashboard/myfavorite"><AiOutlineHeart />My Favorites</NavLink></li>
           <div className="divider"></div>
-          <li><Link to="/"><BiSolidHome />Home</Link></li>
-          <li><Link to="/courses"><SiGotomeeting />Courses</Link></li>
+          <li><NavLink to="/"><BiSolidHome />Home</NavLink></li>
+          <li><NavLink to="/courses"><SiGotomeeting />Courses</NavLink></li>
 
         </ul>
       </div>
